2f1b8ba54988be1d40c0734ecb917bb2bec601b5
Harry Lee <sinyo0523@gmail.com>
2019-01-28 13:21:44 +0000

    D19 | 190128 Midnight | self-study

--- django/exercise/excercise/excercise/urls.py
+++ django/exercise/excercise/excercise/urls.py
@@ -1,4 +1,4 @@
-"""crud URL Configuration
+"""excercise URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from gumy import views
 
 urlpatterns = [
-    path('posts/', include('posts.urls')),
+    path('gumy/', include('gumy.urls')),
     path('admin/', admin.site.urls),
 ]
---
+++ django/exercise2/gumy/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from .models import Student
+
+# Create your views here.
+
+def new(request):
+    return render(request, 'new.html')
+
+
+def create(request):
+    
+    name = request.GET.get('name')
+    age = request.GET.get('age')
+    uni = request.GET.get('uni')
+    
+    student = Student(name=name, age=age, uni=uni)
+    student.save()
+    
+    return render(request, 'create.html')
+
+def index(request):
+    
+    students = Student.objects.all()
+    
+    return render(request, 'index.html', {'students':students})
+    
+    
