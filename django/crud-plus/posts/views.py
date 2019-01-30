dba7b32c0f03f22721c52523877d697efaf7f96e
Harry Lee <sinyo0523@gmail.com>
2019-01-30 03:22:19 +0000

    D21 | 190130 AM | Django URL Names

---
+++ django/crud-plus/posts/views.py
@@ -0,0 +1,66 @@
+from django.shortcuts import render, redirect
+from .models import Post
+
+# Create your views here.
+def new(request):
+    return render(request, 'new.html')
+
+def create(request):
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+    #DB Insert
+    post = Post(title=title, content=content)
+    post.save()
+    
+    return redirect('posts:detail', post.pk)
+    # return redirect(f'/posts/{post.pk}')
+    
+
+def index(request):
+    posts = Post.objects.all()
+    return render(request, 'index.html', {'posts':posts})
+
+
+def detail(request, post_id):
+    post = Post.objects.get(pk=post_id)
+    return render(request, 'detail.html', {'post':post})
+
+
+#Delete
+def delete(request, post_id):
+    post = Post.objects.get(pk=post_id)
+    post.delete()
+    return redirect('posts:list')
+
+
+#Update
+def edit(request, post_id):
+    post = Post.objects.get(pk=post_id)
+    return render(request, 'edit.html', {'post':post})
+
+
+def update(request, post_id):
+    #수정하는 코드
+    post = Post.objects.get(pk=post_id)
+    post.title =request.POST.get('title')
+    post.content =request.POST.get('content')
+    post.save()
+    
+    return redirect('posts:detail', post.pk)
+    #return redirect(f'/posts/{post_id}/')
+
+
+
+
+
+
+
+
+
+
+
+
+def naver(request, q):
+    return redirect(f'https://search.naver.com/search.naver?query={q}')
+
+
